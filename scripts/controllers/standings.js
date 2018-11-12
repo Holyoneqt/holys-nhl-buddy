94130c19035b82a48f64ca005b775e7f181ad3ac
Luca Spühler <luca.spuehler@axa-winterthur.ch>
2018-11-12 15:45:11 +0100

    conferences added

--- scripts/controllers/standings.js
+++ scripts/controllers/standings.js
@@ -1,22 +1,26 @@
 let divisionStandings = [];
+let conferenceStandings = [];
 let leagueStandings = [];
 
 window.onload = () => {
-    document.getElementById('displayDivisions').onclick = () => displayDivisionStandings(divisionStandings);
-    document.getElementById('displayLeague').onclick = () => displayLeagueStandings(leagueStandings);
-
+    document.getElementById('displayDivisions').onclick = () => displayDivisionStandings();
+    document.getElementById('displayConferences').onclick = () => displayConferenceStandings();
+    document.getElementById('displayLeague').onclick = () => displayLeagueStandings();
+    
     getStandings()
         .then(json => divisionStandings = json.records)
         .then(() => displayDivisionStandings())
-        .then(() => leagueStandings = [ ...divisionStandings[0].teamRecords, ...divisionStandings[1].teamRecords, ...divisionStandings[2].teamRecords, ...divisionStandings[3].teamRecords ])
+        .then(() => conferenceStandings = [[...divisionStandings[0].teamRecords, ...divisionStandings[1].teamRecords], [...divisionStandings[2].teamRecords, ...divisionStandings[3].teamRecords]])
+        .then(() => leagueStandings = [...divisionStandings[0].teamRecords, ...divisionStandings[1].teamRecords, ...divisionStandings[2].teamRecords, ...divisionStandings[3].teamRecords])
         .then(() => leagueStandings.sort((a, b) => a.leagueRank - b.leagueRank));
 };
 
 function displayDivisionStandings() {
     document.getElementById('divisions').style.display = 'table';
+    document.getElementById('conferences').style.display = 'none';
     document.getElementById('league').style.display = 'none';
 
-    for(let i = 0; i < divisionStandings.length; i++) {
+    for (let i = 0; i < divisionStandings.length; i++) {
         const divisionContainer = document.getElementById(`division-${i}`);
         divisionContainer.innerHTML = '';
         const division = divisionStandings[i];
@@ -26,19 +30,42 @@ function displayDivisionStandings() {
         title.classList.add('w3-center');
 
         divisionContainer.appendChild(title);
-        divisionContainer.appendChild(createStandingTable(division.teamRecords));
+        divisionContainer.appendChild(createStandingTable(division.teamRecords, true));
+    }
+}
+
+
+function displayConferenceStandings() {
+    document.getElementById('divisions').style.display = 'none';
+    document.getElementById('conferences').style.display = 'table';
+    document.getElementById('league').style.display = 'none';
+
+    const conferenceNames = ['Eastern', 'Western'];
+    for (let i = 0; i < conferenceStandings.length; i++) {
+        const conferenceContainer = document.getElementById(`conference-${i}`);
+        conferenceContainer.innerHTML = '';
+        const conference = conferenceStandings[i];
+        conference.sort((a, b) => a.conferenceRank - b.conferenceRank);
+
+        const title = document.createElement('h3');
+        title.innerHTML = conferenceNames[i];
+        title.classList.add('w3-center');
+
+        conferenceContainer.appendChild(title);
+        conferenceContainer.appendChild(createStandingTable(conference, true));
     }
 }
 
 function displayLeagueStandings() {
     document.getElementById('divisions').style.display = 'none';
+    document.getElementById('conferences').style.display = 'none';
     document.getElementById('league').style.display = 'table';
 
     document.getElementById('league-standing').innerHTML = '';
-    document.getElementById('league-standing').appendChild(createStandingTable(leagueStandings));
+    document.getElementById('league-standing').appendChild(createStandingTable(leagueStandings, true));
 }
 
-function createStandingTable(teamRecords) {
+function createStandingTable(teamRecords, details) {
     const list = document.createElement('ul');
     list.classList.add('w3-ul');
     teamRecords.forEach(t => {
@@ -51,30 +78,35 @@ function createStandingTable(teamRecords) {
         listItem.onclick = () => {
             window.location.href = `./team.html?id=${t.team.id}`;
         }
-        
+
         const img = document.createElement('img');
         img.src = `./images/${t.team.id}.gif`;
-        img.classList.add([ 'w3-bar-item' ]);
+        img.classList.add(['w3-bar-item']);
         img.style.width = '40px';
         img.style.height = '40px';
         img.style.cssFloat = 'left';
         img.style.padding = '8px 2px';
+        listItem.appendChild(img);
 
         const teamName = document.createElement('div');
         teamName.classList.add('w3-bar-item');
-        if (t.wildCardRank === '1') { teamName.style.borderBottom = '2px solid rgba(36, 123, 160, 0.5)'; }
-        else if (t.wildCardRank === '2') { teamName.style.borderBottom = '2px solid rgba(255, 22, 84, 0.5)'; }
+        if (t.wildCardRank === '0') { teamName.style.background = 'rgba(178, 219, 191, 0.5)'; }
+        else if (t.wildCardRank === '1') { teamName.style.background = 'rgba(255, 255, 88, 0.5)'; }
+        else if (t.wildCardRank === '2') { teamName.style.background = 'rgba(255, 22, 84, 0.5)'; }
         teamName.innerHTML = `${TEAM_NAME_SHORT[t.team.name] || t.team.name}`;
+        listItem.appendChild(teamName);
 
         const points = document.createElement('div');
         points.classList.add('w3-bar-item');
         points.style.cssFloat = 'right';
         points.innerHTML = `${t.points} pts.`;
+        listItem.appendChild(points);
 
         const record = document.createElement('div');
         record.classList.add('w3-bar-item');
         record.style.cssFloat = 'right';
         record.innerHTML = `${t.leagueRecord.wins}-${t.leagueRecord.losses}-${t.leagueRecord.ot} (${t.streak.streakCode})`;
+        listItem.appendChild(record);
 
         const plusMinusNumber = t.goalsScored - t.goalsAgainst;
         const plusMinus = document.createElement('div');
@@ -82,12 +114,16 @@ function createStandingTable(teamRecords) {
         plusMinus.style.cssFloat = 'right';
         plusMinus.style.color = plusMinusNumber < 0 ? 'red' : 'green';
         plusMinus.innerHTML = plusMinusNumber < 0 ? plusMinusNumber.toString() : `+${plusMinusNumber}`;
-
-        listItem.appendChild(img);
-        listItem.appendChild(teamName);
-        listItem.appendChild(points);
-        listItem.appendChild(record);
         listItem.appendChild(plusMinus);
+
+        if (details) {
+            const gamesPlayed = document.createElement('div');
+            gamesPlayed.classList.add('w3-bar-item');
+            gamesPlayed.style.cssFloat = 'right';
+            gamesPlayed.innerHTML = `${t.gamesPlayed} GP`;
+            listItem.appendChild(gamesPlayed);
+        }
+
         list.appendChild(listItem);
     });
     return list;
